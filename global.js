1e6a364b35fdaddb56009b1f4e03cdc5b7130d82
Franky <163607462+chillingandcoding@users.noreply.github.com>
2025-10-17 20:39:28 -0700

    Update Website

--- global.js
+++ global.js
@@ -56,6 +56,37 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = event.target.value
 });
 
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+}
+  const data = await response.json();
+  return data;
 
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      ${project.year ? `<p class="year">${project.year}</p>` : ''}
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
 
 
---
+++ index.js
@@ -0,0 +1,26 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+fetchJSON('./lib/projects.json').then((projects) => {
+  const latestProjects = projects.slice(0, 3);
+  const projectsContainer = document.querySelector('.projects');
+  renderProjects(latestProjects, projectsContainer, 'h2');
+});
+
+fetchGitHubData('chillingandcoding').then((githubData) => {
+  const profileStats = document.querySelector('#profile-stats');
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <h2 class="github-stats">GitHub Stats</h2>
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  }
+});
+
+
+
+
